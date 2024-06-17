1fbd4de0c0b9b003e12ad56a97f7d98f4f0fc049
DELL <sulaimabaliqis@gmail.com>
2024-06-17 18:41:28 +0100

    first commit

---
+++ src/test/java/africa/semicolon/com/electionmanagementsystem/ElectionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.com.electionmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
