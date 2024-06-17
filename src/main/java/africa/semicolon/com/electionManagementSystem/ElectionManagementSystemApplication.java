089d996a6201e4e5787a58b8629e53f5a2ff4d30
DELL <sulaimabaliqis@gmail.com>
2024-06-17 22:27:35 +0100

    electionManagement

--- src/main/java/africa/semicolon/com/electionManagementSystem/ElectionManagementSystemApplication.java
+++ src/main/java/africa/semicolon/com/electionManagementSystem/ElectionManagementSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElectionManagementSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ElectionManagementSystemApplication.class, args);
     }
