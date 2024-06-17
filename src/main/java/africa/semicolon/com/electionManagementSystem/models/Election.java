255d4e99dfe7195eb29f75be4be3ec1997c7c148
DELL <sulaimabaliqis@gmail.com>
2024-06-17 22:30:20 +0100

    electionManagement

--- src/main/java/africa/semicolon/com/electionManagementSystem/models/Election.java
+++ src/main/java/africa/semicolon/com/electionManagementSystem/models/Election.java
@@ -1,5 +1,9 @@
 package africa.semicolon.com.electionManagementSystem.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +21,11 @@ public class Election {
     @GeneratedValue(strategy = IDENTITY)
     private Long electionId;
     private String title;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime startTime;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime endTime;
     private Category category;
 }
