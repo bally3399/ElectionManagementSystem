fcc2bb385f46c9a7d7244580dfa7b5651bd02c3c
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-23 22:53:58 +0100

    Me_I_Dont_Understand_AI_Method

--- src/test/java/africa/semicolon/com/electionManagementSystem/controller/AdminControllerTest.java
+++ src/test/java/africa/semicolon/com/electionManagementSystem/controller/AdminControllerTest.java
@@ -1,4 +1,4 @@
-//package africa.semicolon.com.electionManagementSystem.controller;
+package africa.semicolon.com.electionManagementSystem.controller;//package africa.semicolon.com.electionManagementSystem.controller;
 //
 //import africa.semicolon.com.electionManagementSystem.dtos.requests.AddAdminRequest;
 //import africa.semicolon.com.electionManagementSystem.dtos.requests.CancelElectionRequest;
@@ -123,3 +123,71 @@
 //    }
 //
 //}
+
+import africa.semicolon.com.electionManagementSystem.controller.AdminController;
+import africa.semicolon.com.electionManagementSystem.dtos.requests.AddAdminRequest;
+import africa.semicolon.com.electionManagementSystem.dtos.responses.AddAdminResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.is;
+
+
+
+@SpringBootTest
+@Sql(scripts ={"/db/data.sql"})
+public class AdminControllerTest {
+
+    @Autowired
+    private AdminController adminController;
+
+    @Test
+    public void testAddAdminUserAlreadyExist() {
+        AddAdminRequest addAdminRequest = new AddAdminRequest();
+        addAdminRequest.setFirstName("BimBim");
+        addAdminRequest.setLastName("addicted");
+        addAdminRequest.setEmail("admin1@email.com");
+        addAdminRequest.setPassword("password");
+
+        ResponseEntity<AddAdminResponse> response = adminController.addAdmin(addAdminRequest);
+
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddAdminGeneralError() {
+        AddAdminRequest addAdminRequest = new AddAdminRequest();
+        addAdminRequest.setFirstName("BimBim");
+        addAdminRequest.setLastName("addicted");
+        addAdminRequest.setEmail("newadmin@example.com");
+        addAdminRequest.setPassword("password");
+
+        ResponseEntity<AddAdminResponse> response = adminController.addAdmin(addAdminRequest);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    @Test
+    public void testAddAdminSuccess() {
+        AddAdminRequest addAdminRequest = new AddAdminRequest();
+        addAdminRequest.setFirstName("BimBim");
+        addAdminRequest.setLastName("addicted");
+        addAdminRequest.setEmail("newadmin@example.com");
+        addAdminRequest.setPassword("password");
+
+        ResponseEntity<AddAdminResponse> responseEntity = adminController.addAdmin(addAdminRequest);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertThat(responseEntity.getStatusCode(), is(HttpStatus.CREATED));
+    }
+}
+
+
+
+
